2824780487760fb01c21e3e478395c04620463db
ivanpudwill <ivanpudwill22@gmail.com>
2023-11-29 14:23:48 -0500

    sidebar with jquery datatable

--- cookies.js
+++ cookies.js
@@ -1,33 +1,5 @@
-function updateCookieInfo(cookies, tabTitle) { 
-    let activeTabUrl = document.getElementById('header-title');
-    let text = document.createTextNode("Cookies at: " + tabTitle);
 
-    // Clear existing content before adding new content
-    activeTabUrl.innerHTML = "";
-    activeTabUrl.appendChild(text);
-
-    let cookieTable = document.getElementById('cookie-list');
-    
-    //clear rows in table)
-    cookieTable.innerHTML = "";
-
-    if (cookies.length > 0) {
-        for (let cookie of cookies) {
-            let row = cookieTable.insertRow();
-            let cell1 = row.insertCell(0);
-            let cell2 = row.insertCell(1);
-            cell1.innerHTML = cookie.name;
-            cell2.innerHTML = cookie.value;
-        }
-    } else {
-        let row = cookieTable.insertRow();
-        let cell = row.insertCell(0);
-        cell.colSpan = 2;
-        cell.innerHTML = "No cookies in this tab.";
-    }
-
-}
-
+// get Cookies
 function showCookiesForTab(tabs) {
     // get first active tab
     let tab = tabs.pop();
@@ -40,16 +12,39 @@ function showCookiesForTab(tabs) {
         updateCookieInfo(cookies, tab.title);
     });
 }
-  
-//get active tab to run an callback function.
-//it sends to our callback an array of tab objects
+
+// update the cookies in the sidebar 
+function updateCookieInfo(cookies, tabTitle) {
+    let activeTabUrl = $('#header-title');
+    activeTabUrl.html("Cookies at: " + tabTitle);
+
+    let cookieTable = $('#cookie-list').DataTable();
+    cookieTable.clear();
+
+    if (cookies.length > 0) {
+        for (let cookie of cookies) {
+            cookieTable.row.add([cookie.name, cookie.value]).draw();
+        }
+    } else {
+        cookieTable.row.add(["No cookies in this tab.", ""]).draw();
+    }
+}
+
+//get active tab
 function getActiveTab() {
     return browser.tabs.query({currentWindow: true, active: true});
 }
 
-// load cookies when the tab is updated
-browser.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+// Load DataTable when the tab is updated
+browser.tabs.onUpdated.addListener(function (tabId, changeInfo) {
     if (changeInfo.status == "complete") {
         getActiveTab().then(showCookiesForTab);
     }
-})  
+});
+
+// Initialize DataTable when the document is ready
+$(document).ready(function () {
+    $('#cookie-list').DataTable();
+});
+
+
